d5da7cad2a285ba2b36e0d7339b6c3126fbb332e
SharonAbm <136640312+SharonAbm@users.noreply.github.com>
2023-07-12 06:42:06 +0000

    Update LoginPageTest.java

--- src/test/java/testScripts/LoginPageTest.java
+++ src/test/java/testScripts/LoginPageTest.java
@@ -12,8 +12,8 @@ public class LoginPageTest {
 		driver.get("https://the-internet.herokuapp.com/login");
 		driver.findElement(By.id("username")).sendKeys("tomsmith");
 		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-		driver.findElement(By.className("fa fa-2x fa-sign-in")).click();
-		//driver.findElement(By.className(".fa.fa-2x.fa-sign-in")).click();
+		//driver.findElement(By.className("fa fa-2x fa-sign-in")).click();
+		driver.findElement(By.cssSelector(".fa.fa-2x.fa-sign-in")).click();
 		driver.findElement(By.linkText("Elemental Selenium")).click();
 		//driver.findElement(By.partialLinkText("Elemental")).click();
 		//driver.findElement(By.xpath("//input[@type='text']")).sendKeys("tomsmith");
